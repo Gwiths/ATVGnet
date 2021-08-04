d4e81eabea2ae40bdd06cd722454d75bea3916b5
Gwiths <53162830+Gwiths@users.noreply.github.com>
2021-08-04 15:38:05 +0800

    Update models.py

--- code/models.py
+++ code/models.py
@@ -229,30 +229,30 @@ class VG_net(nn.Module):
 
         for i in range(n_downsampling ):
             mult = 2**(n_downsampling-i-1 ) 
-            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),                ## 反卷积层
                                          kernel_size=3, stride=(2),
                                          padding=(1), output_padding=1,
                                          bias=use_bias),
                       norm_layer(int(ngf * mult / 2)),
                       nn.ReLU(True)]
             if i == n_downsampling-3:
-                self.generator1 = nn.Sequential(*model)
+                self.generator1 = nn.Sequential(*model)         ## generator1
                 model = []
 
         self.base = nn.Sequential(*model)
         model = []
         model += [nn.Conv2d(ngf/2, output_nc, kernel_size=7, padding=3)]
         model += [nn.Tanh()]
-        self.generator_color = nn.Sequential(*model)
+        self.generator_color = nn.Sequential(*model)       ## generator_color
 
         model = []
         model += [nn.Conv2d(ngf/2, 1, kernel_size=7, padding=3)]
         model += [nn.Sigmoid()]
-        self.generator_attention = nn.Sequential(*model)
+        self.generator_attention = nn.Sequential(*model)     ## generator_attention
 
         self.bottle_neck = nn.Sequential(conv2d(1024,128,3,1,1))
 
-        
+                                                             ## Conv2dGRU  Convolutional Gated Recurrent Units
         self.convGRU = Conv2dGRU(in_channels = 128, out_channels = 512, kernel_size = (3), num_layers = 1, bidirectional = False, dilation = 2, stride = 1, dropout = 0.5 )
         
     def forward(self,image, landmarks, example_landmark ):
